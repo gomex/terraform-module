a2ead3186588f39806ff046b3e0cc9d1db56b9ad
Rafael Gomes <linux.rafa@gmail.com>
2020-02-21 16:03:06 -0300

    We need the sg id

--- ec2.tf
+++ ec2.tf
@@ -12,7 +12,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  vpc_security_group_ids = aws_security_group.default
+  vpc_security_group_ids = aws_security_group.default.id
 
   tags = {
     Name = "Hello World"
