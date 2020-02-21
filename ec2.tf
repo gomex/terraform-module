31bad3675965101e634d2e9a99a066aaa48551fc
Rafael Gomes <linux.rafa@gmail.com>
2020-02-21 16:39:49 -0300

    We need consistent types on condition

--- ec2.tf
+++ ec2.tf
@@ -12,7 +12,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  vpc_security_group_ids = [var.enable_sg ? aws_security_group.optional[*].id : data.aws_security_group.default.id]
+  vpc_security_group_ids = var.enable_sg ? aws_security_group.optional[*].id : [data.aws_security_group.default.id]
 
   tags = {
     Name = "Hello World"
