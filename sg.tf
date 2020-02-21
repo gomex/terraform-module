c5a7717c94730b88ee16a703914f9d63b880af8a
Rafael Gomes <linux.rafa@gmail.com>
2020-02-21 15:44:05 -0300

    We don't need this prefix

--- sg.tf
+++ sg.tf
@@ -25,6 +25,5 @@ resource "aws_security_group" "allow-traffic" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = ["pl-12c4e678"]
   }
 }
