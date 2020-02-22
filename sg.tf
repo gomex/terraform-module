6547e8892a50b20b005f42c05781028971928205
Rafael Gomes <linux.rafa@gmail.com>
2020-02-22 15:21:22 -0300

    Precisamos de funcionalidade de criar SG

---
+++ sg.tf
@@ -0,0 +1,41 @@
+data "aws_security_group" "default" {
+
+  filter {
+    name   = "group-name"
+    values = ["default"] 
+  }
+
+  tags = {
+    produto   = "default"
+  }
+}
+
+resource "aws_security_group" "optional" {
+  count = var.enable_sg ? 1 : 0
+  name        = "allow-traffic-${var.name}"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingress_ports
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
--- variable.tf
+++ variable.tf
@@ -6,3 +6,15 @@ variable "name" {
   type        = string
   description = "Name of product"
 }
+
+variable "enable_sg" {
+  type        = bool
+  default     = false
+  description = "Habilitar funcionalidade de criação do SG"
+}
+
+variable "ingress_ports" {
+  type        = list(number)
+  default     = []
+  description = "Lista de portas a serem liberadas"
+}
